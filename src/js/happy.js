a12b45a8ec68a1469c46e0cece640ab65f5f4bde
wang970412 <1981789062@qq.com>
2022-10-04 12:37:54 +0800

    gift PC

---
+++ src/js/happy.js
@@ -0,0 +1,31 @@
+$(function () {
+    setTimeout(function () {
+        $('.name').animate({
+            opacity:"1",
+            top:"10%"
+        },2000);
+    },6000);
+
+    setTimeout(function () {
+        $('.happy').animate({
+            opacity:"1",
+            top:"20%"
+        },2000);
+    },6000);
+
+    setTimeout(function () {
+        $('.button-style1').animate({
+            opacity:"1",
+            top:"60%"
+        },1500);
+        $('.button-style2').animate({
+            opacity:"1",
+            top:"70%"
+        },2000);
+    },9000);
+
+    // $(window).click(function(){
+    // 	$('audio')[0].play();
+    // });
+    // window.audio=$('audio')[0].play();
+})
